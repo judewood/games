b395be637c6cd635ba17f9d2c789866296b2b9c5
judewood <judewoodprofessional@gmail.com>
2017-12-02 15:38:41 +0000

    refactoring

--- src/app/games/decoder/dtos/pegs.ts
+++ src/app/games/decoder/dtos/pegs.ts
@@ -3,16 +3,4 @@ export interface Peg {
     id: string;
 }
 
-export class PlainPegs {
-    public pegs: Peg[] = [
-        { id: 'src0', filePath: 'assets/images/redspot.png' },
-        { id: 'src1', filePath: 'assets/images/greenspot.png' },
-        { id: 'src2', filePath: 'assets/images/bluespot.png' },
-        { id: 'src3', filePath: 'assets/images/greyspot.png' },
-        { id: 'src4', filePath: 'assets/images/yellowspot.png' },
-        { id: 'src5', filePath: 'assets/images/blackspot.png' },
-        { id: 'src6', filePath: 'assets/images/purplespot.png' },
-        { id: 'src7', filePath: 'assets/images/orangespot.png' },
-    ];
-}
 
